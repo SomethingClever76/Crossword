a853e4ee6f6dca5b4e20ab420498141713c9a881
Kyle <mrkylethomas76@gmail.com>
2021-01-19 19:48:51 -0500

    Create function -  find_matches - to get possible words

--- xword.py
+++ xword.py
@@ -22,6 +22,19 @@ def make_regex(word):
     return word_regex
 
 
+def find_matches(word_list, word, pattern):
+    "Searches word_list for possible matches"
+    matches = []
+    for item in word_list:
+        if len(item) == len(word) and re.search(pattern, item):
+            matches.append(item)
+    if len(matches) == 0:
+        print('Could not find a word that matches your input.')
+    else:
+        for match in matches:
+            print(match)
+
+
 def main():
     with open('dictionary.txt') as f:
         words = f.read().split()
@@ -30,7 +43,7 @@ def main():
         'Please enter a word to solve.\n' +
         'Use spaces to signify unknown letters: ').lower()
 
-    print(make_regex(test_word))
+    find_matches(words, test_word, make_regex(test_word))
 
 
 if __name__ == '__main__':
